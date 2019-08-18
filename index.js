c96074d0ea8f18a40c26704ee66e65af726e9b41
Alexis Puga Ruíz <alexispuga@pm.me>
2019-08-17 19:29:58 -0500

    Fix ENOENT, add custom cwd, allow .js files and create tmp file if needed
    BUG: Fix ENOENT error by changing the directory in which the jsdoc command is being applied.
    
    ENH: Let the user decide the directory in which the config file is found by passing the "cwd" property to the options of the constructor.
    
    By using a "cwd" option, the user could decide where "./node_modules/jsdoc" lives and still using the "conf" property to specify the config file. That is:
    
    new JsDocPlugin({
    	conf: 'jsdoc/config/jsdoc.conf',
    	cwd: './src'
    });
    
    Means:
    Use the file located at "./src/jsdoc/config/jsdoc.conf".
    Use the command located at "./src/node_modules/jsdoc".
    
    ENH: Add support for CJS modules exported from config files. I.e.:
    Allow "module.exports = ..." in ".js" files.
    
    ENH: Create a temporary file only when needed and remove it afterwards (let the user decide it by passing "preserveTmpFile" to the options of the constructor).
    
    By default, the temporary file is only created if sources are not being taken from the config file. This file adds the ".tmp" suffix and it's removed by default after the compilation is done.

--- index.js
+++ index.js
@@ -1,15 +1,84 @@
-var fs = require('fs');
 var path = require('path');
-var merge = require('lodash/merge');
 var spawn = require('child_process').spawn;
-var fsExtra = require('fs-extra');
+var fse = require('fs-extra');
+var _ = require('lodash');
 
-function Plugin(translationOptions) {
+/** @type {boolean} */
+var isWindows = /^win/.test(process.platform);
+
+/**
+ * Ordered paths to the jsdoc command.
+ *
+ * @type {string[]}
+ * @const
+ */
+var JSDOC_FILES = isWindows ? [
+  'node_modules/.bin/jsdoc.cmd'
+] : [
+  'node_modules/.bin/jsdoc',
+  'node_modules/jsdoc/jsdoc.js'
+];
+
+/**
+ * Looks up for an existing file in each directory.
+ *
+ * @param {string|string[]} [files=[files]] - Filenames in order.
+ * @param {string|string[]} [dirs=[dirs]] - Directories in order.
+ * @returns {?string} The first found file or `null` if nothing is found.
+ */
+var lookupFile = function (files, dirs) {
+  var found = null;
+
+  [].concat(files).some(function (filename) {
+    return [].concat(dirs).some(function (dirname) {
+      var file = path.resolve(path.join(dirname, filename));
+
+      if (fse.existsSync(file)) {
+        return found = file;
+      }
+    });
+  });
+
+  return found;
+};
+
+/**
+ * Reads the jsdoc config file (synchronously) allowing the use of CommonJS
+ * modules or JSON documents as input.
+ *
+ * @param {string} filepath - The filepath to read from.
+ * @throws If the file does not exist or is malformed.
+ * @return {object} The exported value.
+ */
+var readConfigFile = function (filepath) {
+  delete require.cache[filepath];
+  return require(filepath);
+};
+
+function Plugin(options) {
   var defaultOptions = {
-    conf: './jsdoc.conf'
+    /**
+     * Default name for the config file.
+     * A relative path to "cwd" is expected.
+     * @type {?string}
+     */
+    conf: 'jsdoc.conf',
+    /**
+     * Default path for command and file lookup.
+     * @type {?string}
+     */
+    cwd: '.',
+    /**
+     * This option applies only if a config file is not found.
+     * By default, the temp file is removed after the compilation
+     * is done, but you can set this option to a truthy value to
+     * change it.
+     * @type {?boolean}
+     */
+    preserveTmpFile: false
   };
 
-  this.options = merge({}, defaultOptions, translationOptions);
+  this.options = _.merge({}, defaultOptions, options);
 }
 
 Plugin.prototype.apply = function (compiler) {
@@ -22,60 +91,93 @@ Plugin.prototype.apply = function (compiler) {
   });
 
   compiler.plugin('emit', function (compilation, callback) {
+    var cwd = process.cwd();
+    var givenDirectory = options.cwd;
+    var preserveTmpFile = options.preserveTmpFile;
+    var jsdocConfig = path.resolve(givenDirectory, options.conf);
+    var jsdocConfigDir = path.dirname(jsdocConfig);
+    var files = [], jsdocErrors = [];
+    var obj = {};
+    var jsdoc, cmd;
+    var tmpFile;
+
     console.log('JSDOC Start generating');
 
-    fsExtra.readJson(path.resolve(process.cwd(), options.conf), function (err, obj) {
-      var files = [], jsdocErrors = [];
-      var jsdoc, cwd = process.cwd();
+    cmd = lookupFile(JSDOC_FILES, [
+      // 1. Where the config lives.
+      jsdocConfigDir,
+      // 2. In the given directory.
+      givenDirectory,
+      // 3. Where it was called.
+      cwd,
+      // 4. Here.
+      __dirname
+    ]);
 
-      if(err) {
-        callback(err);
+    if (!cmd) {
+      callback(new Error('jsdoc was not found.'));
+      return;
+    }
+
+    if (fse.existsSync(jsdocConfig)) {
+      try {
+        obj = readConfigFile(jsdocConfig);
+      } catch (exception) {
+        callback(exception);
         return;
       }
+    }
 
-      if (obj.source && obj.source.include) {
-        console.log('Taking sources from config file');
-      } else {
-        compilation.chunks.forEach(function (chunk) {
-          chunk.modules.forEach(function (module) {
-            if (module.fileDependencies) {
-              module.fileDependencies.forEach(function (filepath) {
-                files.push(path.relative(process.cwd(), filepath));
-              });
-            }
-          });
+    if (obj.source && obj.source.include) {
+      console.log('Taking sources from config file');
+    }
+    else {
+      compilation.chunks.forEach(function (chunk) {
+        chunk.modules.forEach(function (module) {
+          if (module.fileDependencies) {
+            module.fileDependencies.forEach(function (filepath) {
+              files.push(path.relative(process.cwd(), filepath));
+            });
+          }
         });
-        merge(obj.source, { include: files });
+      });
+      _.merge(obj.source, { include: files });
+
+      tmpFile = jsdocConfig + '.tmp';
+      console.log('Writing temporary file at: ', tmpFile);
+      fse.writeFileSync(tmpFile, JSON.stringify(obj));
+      jsdocConfig = tmpFile;
+    }
+
+    console.log('Using jsdoc located at: ', cmd);
+    jsdoc = spawn(cmd, ['-c', jsdocConfig], {
+      cwd: jsdocConfigDir
+    });
+
+    jsdoc.stdout.on('data', function (data) {
+      console.log(data.toString());
+    });
+
+    jsdoc.stderr.on('data', function (data) {
+      jsdocErrors.push(data.toString());
+    });
+
+    jsdoc.on('close', function (code) {
+      if (tmpFile && !preserveTmpFile) {
+        console.log('Removing the temporary file');
+        fse.unlinkSync(tmpFile);
+        tmpFile = null;
       }
 
-      var jsDocConfTmp = path.resolve(cwd, 'jsdoc.' + Date.now() + '.conf.tmp');
-      fs.writeFileSync(jsDocConfTmp, JSON.stringify(obj));
-
-        if(/^win/.test(process.platform))
-            jsdoc = spawn(path.resolve(cwd) + '/node_modules/.bin/jsdoc.cmd', ['-c', jsDocConfTmp]);
-        else
-            jsdoc = spawn(__dirname + '/node_modules/.bin/jsdoc', ['-c', jsDocConfTmp]);
-
-      jsdoc.stdout.on('data', function (data) {
-        console.log(data.toString());
-      });
-
-      jsdoc.stderr.on('data', function (data) {
-        jsdocErrors.push(data.toString());
-      });
-
-      jsdoc.on('close', function (data, code) {
-        if(jsdocErrors.length > 0) {
-          jsdocErrors.forEach(function (value) {
-            console.error(value);
-          });
-        } else {
-          console.log('JsDoc successful');
-        }
-        fs.unlink(jsDocConfTmp, function() {
-          callback();
+      if(jsdocErrors.length > 0) {
+        jsdocErrors.forEach(function (value) {
+          console.error(value);
         });
-      });
+        callback(new Error('JsDoc exited with code ' + code));
+      } else {
+        console.log('JsDoc successful');
+        callback();
+      }
     });
   });
 
