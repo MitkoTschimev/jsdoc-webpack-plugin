876bbce503fdd6be392b1f95238b03a01c369fe2
Andrew Plummer <plummer.andrew@gmail.com>
2019-10-06 20:13:14 +0900

    Updated to work with webpack compiler hooks

--- index.js
+++ index.js
@@ -16,16 +16,12 @@ Plugin.prototype.apply = function (compiler) {
   var self = this;
   var options = self.options;
 
-  compiler.plugin('watch-run', function (watching, callback) {
-    self.webpackIsWatching = true;
-    callback(null, null);
-  });
-
-  compiler.plugin('emit', function (compilation, callback) {
+  compiler.hooks.afterCompile.tapAsync('JsDoc Webpack Plugin', function (compilation, callback) {
     console.log('JSDOC Start generating');
+    const startTime = new Date();
 
     fsExtra.readJson(path.resolve(process.cwd(), options.conf), function (err, obj) {
-      var files = [], jsdocErrors = [];
+      var files = new Set(), jsdocErrors = [];
       var jsdoc, cwd = process.cwd();
 
       if(err) {
@@ -36,16 +32,18 @@ Plugin.prototype.apply = function (compiler) {
       if (obj.source && obj.source.include) {
         console.log('Taking sources from config file');
       } else {
-        compilation.chunks.forEach(function (chunk) {
-          chunk.modules.forEach(function (module) {
-            if (module.fileDependencies) {
-              module.fileDependencies.forEach(function (filepath) {
-                files.push(path.relative(process.cwd(), filepath));
-              });
-            }
-          });
-        });
-        merge(obj.source, { include: files });
+        obj.source = obj.source || {};
+
+        function pushDependencies(module) {
+          if (module && !files.has(module.resource)) {
+            files.add(module.resource);
+            module.dependencies
+              .map(dep => dep.module)
+              .forEach(pushDependencies);
+          }
+        }
+        compilation.chunks.forEach(chunk => pushDependencies(chunk.entryModule));
+        merge(obj.source, { include: Array.from(files) });
       }
 
       var jsDocConfTmp = path.resolve(cwd, 'jsdoc.' + Date.now() + '.conf.tmp');
@@ -73,16 +71,14 @@ Plugin.prototype.apply = function (compiler) {
           console.log('JsDoc successful');
         }
         fs.unlink(jsDocConfTmp, function() {
+          console.log('JSDOC Finished generating');
+          console.log('JSDOC TOTAL TIME:', new Date() - startTime);
           callback();
         });
       });
     });
   });
 
-  compiler.plugin('done', function (stats) {
-    console.log('JSDOC Finished generating');
-    console.log('JSDOC TOTAL TIME:', stats.endTime - stats.startTime);
-  });
 };
 
 module.exports = Plugin;
